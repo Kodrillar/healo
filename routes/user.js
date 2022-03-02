1e19d6f98667ee212beccb56a53d7ec0bec28804
Kodrillar <ipadeoladavid112@gmail.com>
2022-03-02 03:59:50 +0100

    refactored blog route

--- routes/user.js
+++ routes/user.js
@@ -10,12 +10,14 @@ router.post("/", validateRequestBody(validateSchema), async (req, res)=>{
 
     let user = await User.findOne({email:req.body.email});
     if(user) return res.status(400).json({"message":"User already exist"});
+    
+    const {firstname,lastname,email,password} = req.body 
 
     user = new User({
-        firstname:req.body.firstname,
-        lastname:req.body.lastname,
-        email:req.body.email,
-        password:req.body.password
+        firstname,
+        lastname,
+        email,
+        password,
     })
 
     const salt = await bcrypt.genSalt(10)
