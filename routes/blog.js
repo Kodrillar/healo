9745a49c0295336394afa827f5cdae801d3e8ed0
Kodrillar <ipadeoladavid112@gmail.com>
2022-03-02 04:24:34 +0100

    reverted route refactoring

--- routes/blog.js
+++ routes/blog.js
@@ -7,17 +7,13 @@ const validateObjectId = require("../middleware/objectId")
 
 router.post('/', validate(validateSchema), async(req, res)=>{
 
-    const {title, postBy, image, duration, blogPost} = req.body;
-
-
     let blog = new Blog({
-        title,
-        postBy,
-        image,
-        duration,
-        blogPost
+        title:req.body.title,
+        postBy:req.body.postBy,
+        image:req.body.image,
+        duration:req.body.duration,
+        blogPost:req.body.blogPost
     })
-    
    
     blog = await blog.save();
   
--- routes/user.js
+++ routes/user.js
@@ -10,14 +10,12 @@ router.post("/", validateRequestBody(validateSchema), async (req, res)=>{
 
     let user = await User.findOne({email:req.body.email});
     if(user) return res.status(400).json({"message":"User already exist"});
-    
-    const {firstname,lastname,email,password} = req.body 
 
     user = new User({
-        firstname,
-        lastname,
-        email,
-        password,
+        firstname:req.body.firstname,
+        lastname:req.body.lastname,
+        email:req.body.email,
+        password:req.body.password
     })
 
     const salt = await bcrypt.genSalt(10)
